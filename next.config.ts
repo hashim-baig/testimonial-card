405ee14cb0e3f6a9ddbb5318883779fc71ecf798
Hashim Baig <hashim.baig17@gmail.com>
2025-07-30 00:15:35 +0530

    fix base path

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
-    basePath: isProd ? '/testimonial-card' : '',
-    assetPrefix: isProd ? '/testimonial-card' : '',
+    basePath: '/testimonial-card',
+    assetPrefix: '/testimonial-card',
 
     output: 'export',
 
