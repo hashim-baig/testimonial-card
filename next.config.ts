e17076542df8843e6e4245d8ac66c6707cb81d67
Hashim Baig <hashim.baig17@gmail.com>
2025-07-31 01:04:22 +0530

    fix issue

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    basePath: '/testimonial-card',
-    assetPrefix: '/testimonial-card',
-
-    output: 'export',
-
-    images: {
-        unoptimized: true,
-    },
-
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <>
           <div className="min-h-screen flex items-center justify-center bg-gradient-to-tl from-[#D2D6DB] to-[#F9FAFB]">
                 <TestimonialCard
-                    imgUrl={`${process.env.NEXT_PUBLIC_BASE_PATH || ""}/profile-thumbnail.png`}
+                    imgUrl={`/profile-thumbnail.png`}
                     name={"Sarah Dole"}
                     username={"@sarahdole"}
                     testimonial={"I've been searching for high-quality abstract images for my design projects, and I'm thrilled to have found this platform. The variety and depth of creativity are astounding!"}
